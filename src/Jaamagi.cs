c4efdf9f1f719d0a99305472dcbfd8d17218da5a
Pandemonium14 <104149087+Pandemonium14@users.noreply.github.com>
2026-03-13 22:45:30 +0100

    Prep + Melting Permafrost

---
+++ src/Jaamagi.cs
@@ -0,0 +1,10 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+
+namespace JaamagiAncient.Relics;
+
+
+public class Jaamagi : CustomAncientModel
+{
+    protected override OptionPools MakeOptionPools { get; }
+}
---
+++ src/Relics/MeltingPermafrost.cs
@@ -0,0 +1,33 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models.Relics;
+
+namespace JaamagiAncient.Relics.Relics;
+
+
+public class MeltingPermafrost : CustomRelicModel
+{
+    
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PlatingPower>("PlatingPower",2)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<FocusPower>()];
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext context, CardPlay cardPlay)
+    {
+        if (!CombatManager.Instance.IsInProgress || cardPlay.Card.Owner != Owner || cardPlay.Card.Type != CardType.Power)
+            return;
+        Flash();
+        await PowerCmd.Apply<PlatingPower>(Owner.Creature, DynamicVars["PlatingPower"].BaseValue, Owner.Creature, null);
+    }
+}
