24a72dd66516e55497a7006e181a2b95a07302bf
Pandemonium14 <104149087+Pandemonium14@users.noreply.github.com>
2026-03-14 15:19:34 +0100

    Cold Spark and Ice-Tempered Ingot

---
+++ src/Relics/ColdSpark.cs
@@ -0,0 +1,38 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JaamagiAncient.Relics.Relics;
+
+
+[Pool(typeof(EventRelicPool))]
+public class ColdSpark : CustomRelicModel
+{
+    
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+    
+    private bool usedThisTurn = false;
+
+
+    public override Task AfterBlockGained(Creature creature, decimal amount, ValueProp props, CardModel? cardSource)
+    {
+        if (usedThisTurn || creature.Block < 15) return Task.CompletedTask;
+        usedThisTurn = true;
+        Flash();
+        PlayerCmd.GainEnergy(1, Owner);
+        return Task.CompletedTask;
+    }
+
+    public override Task AfterPlayerTurnStartEarly(PlayerChoiceContext choiceContext, Player player)
+    {
+        usedThisTurn = false;
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Relics/IceTemperedIngot.cs
@@ -0,0 +1,39 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.RelicPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace JaamagiAncient.Relics.Relics;
+
+[Pool(typeof(EventRelicPool))]
+public class IceTemperedIngot : CustomRelicModel
+{
+    public override RelicRarity Rarity => RelicRarity.Ancient;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(12M, ValueProp.Unpowered)];
+
+    private bool shouldTrigger = false;
+    
+    public override Task BeforeTurnEndVeryEarly(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (side != Owner.Creature.Side || Owner.Creature.Block > 0)
+            return Task.CompletedTask;
+        shouldTrigger = true;
+        return Task.CompletedTask;
+    }
+    
+    public override async Task BeforeTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (!shouldTrigger)
+            return;
+        shouldTrigger = false;
+        Flash();
+        Decimal num = await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, null);
+    }
+}
